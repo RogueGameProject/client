1351a82631357c08f37754223e63b1f232b3c1ef
Takaya Ueno <takayaueno0712@gmail.com>
2023-07-12 14:32:14 +1000

    Player Movement Added Mentions: #5

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    // Processing Inputs
+    void Update() {
+        ProcessInputs();
+    }
+
+    // FixedUpdate is called a set amount of times per update move
+    void FixedUpdate() {
+        Move();
+    }
+
+    void ProcessInputs() {
+        // Strictly 0 or 1 for GetAxisRaw
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        /*
+            Every direction normalized, direction speed will not
+            fluctuate under any circumstances 
+        */ 
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move() {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
